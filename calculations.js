4dc1cea28097f42a9b3c495d5432992b3afa7254
Earthbrine <109120240+Earthbrine@users.noreply.github.com>
2024-05-02 11:56:32 -0600

    made radiance work right

--- calculations.js
+++ calculations.js
@@ -459,6 +459,6 @@ function wynnCalc() {
     console.log(aWgi);
     console.log(drW);
     
-    var ehpWynn = Math.floor((1+gW) * ((((hpW + hpWb) * hpMultiplir) * (1 + (ohpW * 0.01)))/((1-ssW) * (1-clssW) * (1-rW) * (1-wsW) * (1-motbW) * (1-bomW) * (1-miW) * (1-dW) * (1-maskMulti) * (1-cotfW) * (1-dWef) * (1-aWgi) * (1-(drW*0.01)))));
+    var ehpWynn = Math.floor((1+gW) * ((((hpW + hpWb) * hpMultiplir) * (1 + (ohpW * 0.01)))/((1-ssW) * (1-clssW) * (1-wsW) * (1-motbW) * (1-bomW) * (1-miW) * (1-dW) * (1-maskMulti) * (1-cotfW) * (1-dWef) * (1-aWgi) * (1-(drW*0.01)))));
     document.getElementById("resultW").innerText = ehpWynn;
 }
